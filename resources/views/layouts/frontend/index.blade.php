cda30fbd88dff7dcdbce87c7cc9113afe175c15f
adityanurdin <adityanurdin0@gmail.com>
2020-02-21 22:03:23 +0700

    add favicon home

--- resources/views/layouts/frontend/index.blade.php
+++ resources/views/layouts/frontend/index.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" 
+      type="image/png" 
+      href="{{asset('img/favicon/favicon-96x96.png')}}">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('bootstrap/dist/css/bootstrap-custom-new.css')}}">
     <!-- Our CSS -->
